157bb567e537d6ec0b7cb3cb81f305bc90044537
Coderboy004 <ashishmishra.dev04@gmail.com>
2022-07-18 14:08:00 +0530

    update some code

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="locations",
-    version="0.0.1",
+    version="1.1.1",
     author="Ashish Mishra",
     author_email="ashishmishra.dev04@gmail.com",
     description="This library will be used to extract usa location",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Coderboy004/locations",
+    project_urls={
+        "Bug Tracker": "https://github.com/Coderboy004/locations/issues"
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
